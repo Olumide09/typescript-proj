a91273c0d0106142ed024eba0a971fc3309d6728
Olumide Olaoye <92790819+Olumide09@users.noreply.github.com>
2024-07-05 15:50:51 -0500

    Added functionality of the login pages

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './login-pages/Home';
+import Login from './login-pages/Login';
+import Register from './login-pages/Register';
 
 
 function App() {
   return (
     <>
+    <Routes>
+      <Route path="/" element={<Home />} />    
+      <Route path="/login" element={<Login />} />  
+      <Route path="/register" element={<Register />} />  
+    </Routes>
     </>
   );
 }
